cd9c0d74d064e6f0dcfc7fd111a5e79cbb3b06e6
YouriMartin <youri.martin@gmail.com>
2026-01-05 20:58:57 +0100

    integrate EpicModule into app module

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,6 +2,7 @@ import {Module} from '@nestjs/common';
 import {ConfigModule} from '@nestjs/config';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
+import {EpicModule} from './modules/epic.module';
 import {UserStoryModule} from './modules/user-story.module';
 import {CodeRepositoryModule} from './modules/code-repository.module';
 import {DatabaseModule} from './infrastructure/database/database.module';
@@ -15,6 +16,7 @@ import appConfig from './infrastructure/config/app.config';
       load: [kyselyConfig, appConfig],
     }),
     DatabaseModule,
+    EpicModule,
     UserStoryModule,
     CodeRepositoryModule,
   ],
